62aa4b99ee0ea0aaaa687a14b875e649b56e895b
Linus Braun <lbraun0203@gmail.com>
2025-05-09 06:54:20 +0000

    tetet

--- server.py
+++ server.py
@@ -4,40 +4,8 @@ import time
 # import paho.mqtt.client as mqtt           Wurde bei Installation von flask mqtt deinstalliern
 import flask_mqtt as mqtt
 
-
-
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(main_routes)
-
-
-app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
-app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
-app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
-app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
-topic = '/flask/mqtt'
-
-mqtt_client = Mqtt(app)
-
-
-@mqtt_client.on_connect()
-def handle_connect(client, userdata, flags, rc):
-   if rc == 0:
-       print('Connected successfully')
-       mqtt_client.subscribe(topic) # subscribe topic
-   else:
-       print('Bad connection. Code:', rc)
-
-@app.route('/publish', methods=['POST'])
-def publish_message():
-   request_data = request.get_json()
-   publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
-   return jsonify({'code': publish_result[0]})
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000, threaded=True)
