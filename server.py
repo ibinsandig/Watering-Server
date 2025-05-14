dc43d30e3ead3bc924ab8bcad96e54fbf09c8429
Linus Braun <lbraun0203@gmail.com>
2025-05-14 08:09:11 +0000

    Enhance MQTT integration and update HTML templates for message display

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from routes.main import main_routes
 import time
 import paho.mqtt.client as mqtt
 from flask_socketio import SocketIO
+import threading
+
 
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(main_routes)
@@ -21,16 +23,20 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
     # Emit the received data to all connected web clients
-    socketio.emit('mqtt_message', {'topic': msg.topic, 'payload': msg.payload.decode()})
+    #   socketio.emit('mqtt_message', {'topic': msg.topic, 'payload': msg.payload.decode()})
 
 # Set up MQTT client
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
+
 
 # Connect to MQTT broker
-mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-mqtt_client.loop_start()
+def connect_mqtt():
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message 
+    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    mqtt_client.loop_start()            
 
+# threading.Thread(target=connect_mqtt).start()
+ 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=3000)
